3d701abe0c1296c86e95b3abf4e801754891ad2b
tuj55883 <78548481+tuj55883@users.noreply.github.com>
2021-03-26 17:25:11 -0400

    Added the book list class

---
+++ app/src/main/java/edu/temple/assignment7/BookList.java
@@ -0,0 +1,30 @@
+package edu.temple.assignment7;
+
+import java.util.ArrayList;
+
+
+public class BookList {
+    private ArrayList<Book> list;
+
+
+    public BookList(){
+    }
+
+    public void add(Book x){
+        list.add(x);
+    }
+
+    public void remove(Book x){
+        list.remove(x);
+    }
+
+    public Book get(int x){
+        return list.get(x);
+    }
+
+    public void size(){
+        list.size();
+    }
+
+
+}
