afbb0d502b997ba945027b600680031218d0898a
tuj55883 <78548481+tuj55883@users.noreply.github.com>
2021-03-26 17:12:27 -0400

    Added the book class

--- app/src/main/java/edu/temple/assignment7/Book.java
+++ app/src/main/java/edu/temple/assignment7/Book.java
@@ -1,4 +1,19 @@
 package edu.temple.assignment7;
 
 public class Book {
+    private String title;
+    private String author;
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
 }
